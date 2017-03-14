00e8ea9589553d8f6826c2b2e88fe3c305b93547
Tsigie Kumssa <tktsigie@yahoo.com>
2017-03-14 15:37:58 -0400

    TeiviaGame

---
+++ assets/javascript/app.js
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+<meta charset="utf-8">
+	<title>Trivia Game</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+<body>
+
+<div id="wrapper">
+	<div id="display">
+		<h1>Totally Trivial Trivia!</h1>
+			<div id="Time">Time Remaining: 28 Seconds</div>
+				<h3>What was the first full length CGI movie?</h3>
+					<div id="movies">
+						<div class="movie">A Bug's Life</div>
+						<div class="Movie">Memory Loss</div>
+						<div class="movie">Toy Story</div>
+						<div class="movie">The Lion King</div>
+					</div>
+				<h3>Which of these is NOT a name of one of the Spice Girls?</h3>
+		<div id="Spice Girls">
+			<div class="spice girl">Melanie Brown</div>
+			<div class="spice girl">Victoria Beckham</div>
+			<div class="spice girl">Emma Bunton</div>
+			<div class="spice girl">Geri Halliwell</div>
+			<div class="spice girl">Melanie Chisholm</div>
+		</div>
+			<div id="NBAteam"><h3>Which NBA team won the most titles in the 90s?</h3>
+				<div class="team">Washington Bullets</div>
+				<div class="team">Los Angeles Lakers</div>
+				<div class="team">Chicago Bulls</div>
+				<div class="team">New York Knicks</div>
+				<div class="team">Portland Trail Blazers</div>
+			</div>
+
+
+
+	</div>
+</div>
+	</script>
+</body>
+</html>
